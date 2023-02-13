d33b8da34351a968cfeb79c909517040a4cb9aad
Enzo_Ipanaque <enzo.fernando.ipana1@managementsolutions.com>
2023-02-13 16:58:19 -0500

    V1.3

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='MotorDeCalidadLocal',
+version='1.3',
+description='Paquete demo de Motor de Calidad Version Local',
+author='Enzo Ipanaque',
+author_email='enzo.ipanaque@ms-peru.com',
+license='Management Solutions',
+packages=['motordecalidadlocal'],
+py_modules=['functions','constants','rules'],
+zip_safe=False)
